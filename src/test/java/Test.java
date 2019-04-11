99f8e12882223ccb4b68f45538337ffcd9803c0a
matgry97 <mateusz.14@hotmail.com>
2019-04-11 19:29:44 +0200

    Første Commit med Test om testene funker

--- src/test/java/Test.java
+++
@@ -1,7 +0,0 @@
-import org.junit.jupiter.params.ParameterizedTest;
-
-public class Test {
-
-
-
-}
---
+++ src/test/java/Testing.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class Testing {
+
+    @BeforeAll
+    private ArrayList<Film>
+
+    @Test
+
+
+}
