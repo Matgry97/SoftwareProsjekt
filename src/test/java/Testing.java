96f9f6f2275eb79a2a7625895267e62db5f6e067
dagvik <dag-eirik@hotmail.com>
2019-04-11 20:54:33 +0200

    sjekk mateusz

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -3,19 +3,21 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class Testing {
 
-    @BeforeAll
-    private ArrayList<Film> filmer;
-
     @Test
-    public void testAddFilm(Film film) {
-        filmer.add(film);
-        Assertions.assertEquals(filmer.get(0));
+    public void testAddFilm() {
+        Kino ålesundKino = new Kino(4);
+        Film harryPotter = new Film("Harry Potter", "magisk film", 100, LocalDate.of(2002, 12, 05));
+        assertNotNull(ålesundKino.leggTilFilm(harryPotter));
+
     }
 
 
